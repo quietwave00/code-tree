e26b66ff87bd4fc1d2a55a42e761777c3941d19d
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-14 16:36:32 +0900

    Title: 줄 세우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241014/줄 세우기/line-up-students.cpp
+++ 241014/줄 세우기/line-up-students.cpp
@@ -4,26 +4,39 @@
 
 using namespace std;
 
+class Student {
+    public:
+        int h, w, num;
+    
+    Student() {}
+    Student(int h, int w, int num) {
+        this->h = h;
+        this->w = w;
+        this->num = num;
+    }
+};
 
+bool cmp(Student a, Student b) {
+    if (a.h != b.h) return a.h > b.h;
+    if (a.w != b.w) return a.w > b.w;
+    return a.num < b.num;
+}
 
 int main() {
     int n;
     cin >> n;
 
-    tuple<int, int, int> persons[n];
+    Student students[n];
     for(int i = 0; i < n; i++) {
         int h, w;
         cin >> h >> w;
-        persons[i] = make_tuple(-h, w, i + 1);
-        
+        students[i] = Student(h, w, i + 1);
     }
 
-    sort(persons, persons + n);
+    sort(students, students + n, cmp);
 
     for(int i = 0; i < n; i++) {
-        int h, w, num;
-        tie(h, w, num) = persons[i];
-        printf("%d %d %d\n", -h, w, num);
+        printf("%d %d %d\n", students[i].h, students[i].w, students[i].num);
     }
      
     return 0;
