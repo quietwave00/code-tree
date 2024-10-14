e213d5ef3f9e4ba5c678164a641a2fce78ed5abf
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-14 16:16:30 +0900

    Title: 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/줄 세우기/line-up-students.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+
+using namespace std;
+
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    tuple<int, int, int> persons[n];
+    for(int i = 0; i < n; i++) {
+        int h, w;
+        cin >> h >> w;
+        persons[i] = make_tuple(-h, w, i + 1);
+        
+    }
+
+    sort(persons, persons + n);
+
+    for(int i = 0; i < n; i++) {
+        int h, w, num;
+        tie(h, w, num) = persons[i];
+        printf("%d %d %d\n", h, w, num);
+    }
+     
+    return 0;
+}
