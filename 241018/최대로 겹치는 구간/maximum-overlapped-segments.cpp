192e826224d8e46c3bfda70e6a81982c4d6754df
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-18 00:38:55 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241018/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 241018/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,27 +1,54 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
 
-    int line[101] = { 0 };
-    for(int i = 1; i <= n; i++) {
+    int arr[201] = { 0 };
+    vector<pair<int, int>> vec;
+    int flag = false;
+    for(int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        for(int j = a; j < b; j++) {
-            line[j]++;
+
+        if(a < 0 || b < 0) flag = true;
+        vec.push_back(make_pair(a, b));
+    }
+
+    // offset 작업
+    if(flag) {
+        int min = 0;
+        for(int i = 0; i < n; i++) {
+            if(vec[i].first < min) {
+                min = vec[i].first;
+            }
+        }
+
+        int offset = -min;
+        for(int i = 0; i < n; i++) {
+            vec[i].first += offset;
+            vec[i].second += offset;
+        }
+    }
+    
+    // 구간 색칠
+    for(int i = 0; i < n; i++) {
+        for(int j = vec[i].first; j < vec[i].second; j++) {
+            arr[j]++;
         }
     }
 
-    
-
-    int cnt = 0;
-    for(int i = 1; i <= 101; i++) {
-        if(line[i] >= 2) cnt++;
+    int max = 0;
+    for(int i = 1; i <= 200; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
     }
 
-    cout << cnt << '\n';
+    cout << max << '\n';
     
     return 0;
 }
