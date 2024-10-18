92f4347268a5e7623bf24c1c231334fa6f67de6b
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-18 17:04:23 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241018/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int MAX = 10 * n + 1;
+    int arr[MAX] = { 0 };
+    int cur = 10 * n / 2;
+    for(int i = 0; i < n; i++) {
+        int x;
+        char cmd;
+        cin >> x >> cmd;
+
+        if(cmd == 'R') {
+            for(int j = cur; j < cur + x; j++) {
+                arr[j]++;
+            }
+            cur += x;
+        }
+        else {
+            for(int j = cur; j > cur - x; j--) {
+                arr[j]++;
+            }
+            cur -= x;
+        }
+
+    }
+
+    int cnt = 0;
+    for(int i = 1; i < MAX; i++) {
+        if(arr[i] >= 2) cnt++;
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
