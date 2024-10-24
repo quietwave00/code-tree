4a5ab5ac2ba29470187c5abb5fb42650f4e4252b
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-24 14:37:18 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/연속되는 수 2/continuous-number2.cpp
+++ 241024/연속되는 수 2/continuous-number2.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    int arr[n];
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
+    vector<int> numbers(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> numbers[i];
     }
 
-    int cnt = 0;
-    int cnt_arr[n] = { 0 };
-    for(int i = 0; i < n; i++) {
-        if(i == 0 || arr[i] != arr[i - 1]) {
-            cnt++;
-            cnt_arr[i] = cnt;
+    int max_count = 1;
+    int current_count = 1;
+
+    for (int i = 1; i < N; ++i) {
+        if (numbers[i] == numbers[i - 1]) {  // 이전 숫자와 같으면
+            current_count++;
+        } else {
+            max_count = max(max_count, current_count);
+            current_count = 1;
         }
     }
 
-    sort(cnt_arr, cnt_arr + n);
+    max_count = max(max_count, current_count);
 
-    cout << cnt_arr[n -1];
+    cout << max_count << endl;
 
     return 0;
 }
