47d9590a3094a4f1c80e444f094d1d19b1dac86a
Nari <51073778+quietwave00@users.noreply.github.com>
2024-10-28 17:57:02 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241028/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 2000
+#define OFFSET 1000
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int time_a[MAX] = { 0 };
+    int time_b[MAX] = { 0 };
+    int cur_a = OFFSET, cur_b = OFFSET;
+    int current_time = 1; 
+
+    for (int i = 0; i < n; i++) {
+        char cmd;
+        int t;
+        cin >> cmd >> t;
+
+        for (int j = 0; j < t; j++) {
+            if (cmd == 'R') {
+                cur_a += 1;
+            } else {
+                cur_a -= 1;
+            }
+            time_a[cur_a] = current_time++;
+        }
+    }
+
+    current_time = 1;
+
+    // B의 이동 기록
+    for (int i = 0; i < m; i++) {
+        char cmd;
+        int t;
+        cin >> cmd >> t;
+
+        for (int j = 0; j < t; j++) {
+            if (cmd == 'R') {
+                cur_b += 1;
+            } else {
+                cur_b -= 1;
+            }
+            time_b[cur_b] = current_time++; 
+        }
+    }
+
+    for (int i = 0; i < MAX; i++) {
+        if (time_a[i] != 0 && time_b[i] != 0 && time_a[i] == time_b[i]) {
+            cout << time_a[i]; 
+            break;
+        }
+    }
+
+    return 0;
+}
