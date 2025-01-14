bd70a838a00bfe518ccac3529e8915eb756534e7
Nari <51073778+quietwave00@users.noreply.github.com>
2025-01-14 23:56:53 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int A[100];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    
+    int answer = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            if (A[i] < A[j]) {
+                for (int k = j + 1; k < N; k++) {
+                    if (A[j] < A[k]) {
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << answer << endl;
+    
+    return 0;
+}
