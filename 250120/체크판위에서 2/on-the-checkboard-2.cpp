a0bb78cb53da1c612e379f40f98138b01de4fa4b
Nari <51073778+quietwave00@users.noreply.github.com>
2025-01-20 23:23:33 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250120/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define MAX_N 15
+
+using namespace std;
+
+int n, m;
+char grid[MAX_N][MAX_N];
+
+int main() {
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> grid[i][j];
+    
+    int cnt = 0;
+    for(int i = 1; i < n; i++)
+        for(int j = 1; j < m; j++)
+            for(int k = i + 1; k < n - 1; k++)
+                for(int l = j + 1; l < m - 1; l++)
+                    if(grid[0][0] != grid[i][j] && 
+                       grid[i][j] != grid[k][l] &&
+                       grid[k][l] != grid[n - 1][m - 1])
+                        cnt++;
+                        
+    cout << cnt;
+    return 0;
+}
