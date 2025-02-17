64ab1850fb02959b0ca7034d6f3922af75a65c89
Nari <51073778+quietwave00@users.noreply.github.com>
2025-02-17 14:46:51 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250217/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[20];
+
+bool isValid(int a, int b) {
+    int digit = 1;
+    while (digit <= max(a, b)) {
+        int digit1 = (a / digit) % 10;
+        int digit2 = (b / digit) % 10;
+
+        if (digit1 + digit2 >= 10) return false; // carry 발생 시 실패
+        digit *= 10;
+    }
+    return true;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max_num = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (!isValid(arr[i], arr[j])) continue; // 두 수의 carry 발생 체크
+
+            for (int k = j + 1; k < n; k++) {
+                if (isValid(arr[i] + arr[j], arr[k])) { // 세 번째 숫자까지 carry 체크
+                    max_num = max(max_num, arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+    }
+
+    cout << max_num << endl;
+    return 0;
+}
