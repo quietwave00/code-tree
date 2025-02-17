a990dd767a0e842bd2f11bb729c2fadbea50cc21
Nari <51073778+quietwave00@users.noreply.github.com>
2025-02-17 14:51:19 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250217/Carry 피하기 2/escaping-carry-2.cpp
+++ 250217/Carry 피하기 2/escaping-carry-2.cpp
@@ -12,7 +12,7 @@ bool isValid(int a, int b) {
         int digit1 = (a / digit) % 10;
         int digit2 = (b / digit) % 10;
 
-        if (digit1 + digit2 >= 10) return false; // carry 발생 시 실패
+        if (digit1 + digit2 >= 10) return false; // carry 발생
         digit *= 10;
     }
     return true;
@@ -25,7 +25,7 @@ int main() {
     }
 
     int max_num = 0;
-
+    bool valid = false;
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (!isValid(arr[i], arr[j])) continue; // 두 수의 carry 발생 체크
@@ -33,11 +33,14 @@ int main() {
             for (int k = j + 1; k < n; k++) {
                 if (isValid(arr[i] + arr[j], arr[k])) { // 세 번째 숫자까지 carry 체크
                     max_num = max(max_num, arr[i] + arr[j] + arr[k]);
+                    valid = true;
                 }
             }
         }
     }
 
-    cout << max_num << endl;
+    if (!valid) cout << -1 << endl;
+    else cout << max_num << endl;
+
     return 0;
 }
