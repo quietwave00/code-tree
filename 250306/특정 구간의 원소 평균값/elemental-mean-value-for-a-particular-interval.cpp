c6c2e657e85df0e92a74d60f48082623ac1ae35c
Nari <51073778+quietwave00@users.noreply.github.com>
2025-03-06 23:39:47 +0900

    Title: 특정 구간의 원소 평균값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250306/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int arr[101];
+
+bool Includes(int start, int end, int num) {
+    // cout << "num: " << num << endl;
+    for(int i = start; i <= end; i++) {
+        if(arr[i] == num) return true;
+    }
+    return false;
+}
+
+int main() {
+    cin >> N;
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    
+    int cnt = 0;
+    for(int size = 1; size <= N; size++) {
+        for(int i = 1; i <= N - size + 1; i++) {
+            int sum = 0;
+            for(int j = 0; j < size; j++) {
+                sum += arr[i + j];
+            }
+            if(sum % size == 0) { 
+                int avg = sum / size;
+                if(Includes(i, i + size - 1, avg)) cnt++;
+            }
+        }
+        // cout << "=======" << endl;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
+
+// 구간 size: 1~N
+// 시작 int i = 0, 끝 i < N
