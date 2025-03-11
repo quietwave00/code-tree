3c1655024d86ac561ac73d90d5ad5a922bd09fc4
Nari <51073778+quietwave00@users.noreply.github.com>
2025-03-11 16:38:09 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250311/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+map<int, int> m;
+
+bool Includes(int x) {
+    return x >= 0 && x <= 100;
+}
+
+int main() {
+    cin >> N >> K;
+
+    for(int i = 0; i < N; i++) {
+        int num1 = 0, num2 = 0;
+        cin >> num1 >> num2;
+        m.insert({num2, num1});
+    }
+
+    int max_num = 0;
+    for(int i = 0; i <= 100; i++) {
+        int sum = 0;
+        if(Includes(i - K) && Includes(i + K)) {
+            // cout << "i - K:" << i - K << ", i + K: " << i + K << endl;
+            for(int j = i - K; j <= i + K; j++) {
+                // cout << j << ": " << m[j] << endl;
+                sum += m[j];
+            }
+        max_num = max(sum, max_num);
+        }
+    }
+
+    cout << max_num << endl;
+    
+    return 0;
+}
