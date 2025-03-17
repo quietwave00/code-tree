83a3d20adfbe53b77b0172c6a390552dec307fc8
Nari <51073778+quietwave00@users.noreply.github.com>
2025-03-17 13:36:40 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250317/개발자의 능력 2/ability-of-developer-2.cpp
+++ 250317/개발자의 능력 2/ability-of-developer-2.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <climits>
+#include <vector>
 
 using namespace std;
 
 int arr[6];
-vector<int> vec;
 
 int main()
 {
@@ -15,20 +14,50 @@ int main()
         cin >> arr[i];
     }
 
-    int min_num = INT_MAX;
+    int min_diff = INT_MAX;
+    int sum1 = 0, sum2 = 0;
+    // 첫 번째
     for (int i = 0; i < 6; i++)
     {
         for (int j = i + 1; j < 6; j++)
         {
-            int sum = arr[i] + arr[j];
-            vec.push_back(sum);
+            sum1 = arr[i] + arr[j];
+            // 두 번째
+            for(int k = 0; k < 6; k++) {
+                if (k == i || k == j) continue;
+                for(int l = k + 1; l < 6; l++) {
+                    if(l == i || l == j) continue;
+                    sum2 = arr[k] + arr[l];
+
+                    int nums[6] = {0};
+                    nums[i] = 1; nums[j] = 1; nums[k] = 1; nums[l] = 1;
+
+                    // 세 번째
+                    int sum3 = 0;
+                    for (int m = 0; m < 6; m++)
+                    {
+                        if (!(nums[m] == 1))
+                        {
+                            sum3 += arr[m];
+                        }
+                    }
+                    vector<int> sums;
+                    sums.push_back(sum1);
+                    sums.push_back(sum2);
+                    sums.push_back(sum3);
+
+                    // printf("%d %d %d\n", sum1, sum2, sum3);
+
+                    sort(sums.begin(), sums.end());
+
+                    int diff = sums[2] - sums[0];
+                    min_diff = min(diff, min_diff);
+                }
+            }
         }
     }
 
-    sort(vec.begin(), vec.end());
-
-    int idx = vec.size() - 1;
-    cout << vec[idx] - vec[idx - 2] << endl;
+    cout << min_diff << endl;
 
     return 0;
 }
