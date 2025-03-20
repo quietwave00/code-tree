7ad829127e47bdbbe91389df9d279847213d8740
Nari <51073778+quietwave00@users.noreply.github.com>
2025-03-20 16:28:08 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250320/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int arr[100];
+
+int main()
+{
+    cin >> N >> K;
+    for(int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int max_num = 0;
+    for (int target = 0; target < N - 2; target++) {
+        int check[1000] = { 0 };
+        for (int i = target; i <= target + K; i++) {
+            check[arr[i]]++;
+        }
+        for (int i = 0; i < 1000; i++) {
+            // printf("check[%d]: %d\n", i, check[i]);
+            if(check[i] >= 2) max_num = max(i, max_num);
+        }
+    }
+
+    cout << max_num << endl;
+
+    return 0;
+}
+
+// 폭탄 N개, K씩 범위 잡아보기, 범위 안에 같은 번호 있는지 체크, max값 update
