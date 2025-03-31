037309af2580a5a76fd8d9686ff6f83f7962c4dc
Nari <51073778+quietwave00@users.noreply.github.com>
2025-03-31 15:00:53 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250331/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250331/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #define MAX_N 100
 #define MAX_M 99
 
@@ -9,21 +8,31 @@ int arr[MAX_N][MAX_N];
 using namespace std;
 
 int CheckCol(int x) {
-    int cnt = 1;
+    int cnt = 1, max_cnt = 1;
     for(int t = 0; t < N - 1; t++) {
-        if(arr[t][x] == arr[t + 1][x]) cnt++;
-        else cnt = 1;
+        // printf("arr[%d][%d]: %d, arr[%d][%d]: %d\n", t, x, arr[t][x], t + 1, x, arr[t + 1][x]);
+        if(arr[t][x] == arr[t + 1][x]) {
+            cnt++;
+            max_cnt = max(max_cnt, cnt);
+        } else {
+            cnt = 1;
+        }
     }
-    return cnt;
+    return max_cnt;
 }
 
 int CheckRow(int x) {
-    int cnt = 1;
+    int cnt = 1, max_cnt = 1;
     for(int t = 0; t < N - 1; t++) {
-        if(arr[x][t] == arr[x][t + 1]) cnt++;
-        else cnt = 1;
+        // printf("arr[%d][%d]: %d, arr[%d][%d]: %d\n", x, t, arr[x][t], x, t + 1, arr[x][t + 1]);
+        if(arr[x][t] == arr[x][t + 1]) {
+            cnt++;
+            max_cnt = max(max_cnt, cnt);
+        } else {
+            cnt = 1;
+        }
     }
-    return cnt;
+    return max_cnt;
 }
 
 int main() {
@@ -35,18 +44,18 @@ int main() {
     }
 
     int total_cnt = 0;
+
     // 행 체크
-    int row_cnt = 0;
     for(int i = 0; i < N; i++) {
         int row_cnt = CheckRow(i);
+        // cout << "row_cnt: " << row_cnt << endl;
         if(row_cnt >= M) total_cnt += 1;
     }
-
-    // cout << "==================" << endl;
 
     // 열 체크
     for(int i = 0; i < N; i++) {
         int col_cnt = CheckCol(i);
+        // cout << "col_cnt: " << col_cnt << endl;
         if(col_cnt >= M) total_cnt += 1;
     }
 
@@ -54,6 +63,3 @@ int main() {
 
     return 0;
 }
-
-// N * N 행마다 체크, 열마다 체크해서 해당 수열 개수
-
