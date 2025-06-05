9a279841173c567c9bbc790a86be01a9e3c329ab
Nari <51073778+quietwave00@users.noreply.github.com>
2025-06-05 16:44:07 +0900

    Title: 전부 겹치는 선분, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250605/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 250605/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
 
 int N;
-int arr[101];
+pair<int, int> arr[100];
 
 int main() {
     cin >> N;
     for(int i = 0; i < N; i++) {
         int num1, num2;
         cin >> num1 >> num2;
-        
-        for(int j = num1; j <= num2; j++) {
-            arr[j] += 1;
-        }
+        arr[i] = make_pair(num1, num2);
     }
 
     bool overlap = false;
-    for(int i = 1; i <= 100; i++) {
-        if(arr[i] == N) overlap = true;
+    // 제거 선택
+    for(int i = 0; i < N; i++) {
+        int max_x1 = 0;
+        int min_x2 = INT_MAX;
+        for(int j = 0; j < N; j++) {
+            if(j == i) continue;
+
+            max_x1 = max(arr[j].first, max_x1);
+            min_x2 = min(arr[j].second, min_x2);
+        }
+        // printf(">>>> min_start: %d, max_end: %d\n", min_start, max_end);
+        // cout << "=========" << endl;
+        if(min_x2 < max_x1) overlap = false;
+        else overlap = true;
     }
 
     if(overlap) cout << "Yes";
     else cout << "No";
-
     
     return 0;
 }
 
-// 모두 겹치는 지점 있는지
+
+// 겹치게 만들기
+// min_start > max_end
