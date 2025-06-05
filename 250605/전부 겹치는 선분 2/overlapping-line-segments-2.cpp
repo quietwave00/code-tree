c2cc66bfa0898eab6ec6832a5daeb95e350aa077
Nari <51073778+quietwave00@users.noreply.github.com>
2025-06-05 16:48:00 +0900

    Title: 전부 겹치는 선분 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250605/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int N;
+pair<int, int> arr[100];
+
+int main() {
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        int num1, num2;
+        cin >> num1 >> num2;
+        arr[i] = make_pair(num1, num2);
+    }
+
+    bool overlap = false;
+    // 제거 선택
+    for(int i = 0; i < N; i++) {
+        int max_x1 = 0;
+        int min_x2 = INT_MAX;
+        for(int j = 0; j < N; j++) {
+            if(j == i) continue;
+
+            max_x1 = max(arr[j].first, max_x1);
+            min_x2 = min(arr[j].second, min_x2);
+        }
+        // printf(">>>> min_start: %d, max_end: %d\n", min_x2, max_x1);
+        // cout << "=========" << endl;
+        if(min_x2 < max_x1) overlap = false;
+        else {
+            overlap = true;
+            break;
+        }
+    }
+
+    if(overlap) cout << "Yes";
+    else cout << "No";
+    
+    return 0;
+}
+
+
+// 겹치게 만들기
+// min_start > max_end
