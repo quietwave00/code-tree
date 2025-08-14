5b59a8df0d7484981de183d167983666e495ced1
Nari <51073778+quietwave00@users.noreply.github.com>
2025-08-14 10:31:09 +0900

    Title: 겹치지 않게 선분 고르기, Time: 1069ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 250814/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 250814/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -11,10 +11,8 @@ bool IsOverlap() {
     for(int i = 0; i < comp.size(); i++) {
         for(int j = line[comp[i]].first; j <= line[comp[i]].second; j++) {
             check[j]++;
+            if(check[j] > 1) return true;
         }
-    }
-    for(int i = 0; i < 1000; i++) {
-        if(check[i] > 1) return true;
     }
     return false;
 }
@@ -54,4 +52,4 @@ int main() {
 
 // 주어진 영역에서 겹치지 않게 선택...
 // 하나씩 다 조합해보고 겹치는지 안 겹치는지 체크. 겹치면 return false;
-// 그리고 max계산하면 될듯!!
+// 그리고 cnt구한 후 max계산하면 될듯!!
