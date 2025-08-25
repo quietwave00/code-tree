db999a35ad8413aef8a4cbda6635618e037c0647
Nari <51073778+quietwave00@users.noreply.github.com>
2025-08-25 21:17:46 +0900

    Title: 알파벳과 사칙연산, Time: 114ms, Memory: 0MB, Status: Passed - Codetree

--- 250825/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 250825/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -2,58 +2,53 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <map>
 using namespace std;
 
 string str;
 int max_num = INT_MIN;
 vector<int> comp;
+vector<char> alpha;
+map<char,int> idx_map; // 알파벳 -> comp
 
 int GetResult() {
-    int result = comp[0];
-    int comp_idx = 1;
-    
-    for(int i = 1; i < str.length(); i += 2) {
+    int result = comp[idx_map[str[0]]];
+    for (int i = 1; i < str.length(); i += 2) {
         char op = str[i];
-        int next_num = comp[comp_idx++]; 
+        int next_num = comp[idx_map[str[i+1]]];
         
-        if(op == '+') {
-            result += next_num;
-        }
-        else if(op == '-') {
-            result -= next_num;
-        }
-        else if(op == '*') {
-            result *= next_num;
-        }
+        if (op == '+') result += next_num;
+        else if (op == '-') result -= next_num;
+        else if (op == '*') result *= next_num;
     }
     return result;
 }
 
-void Choose(int pos) {
-    if(pos >= str.length()) {
+void Choose() {
+    if (comp.size() == alpha.size()) {
         int result = GetResult();
         max_num = max(result, max_num);
         return;
     }
     
-    if(pos % 2 == 1) {
-        Choose(pos + 1);
-        return;
-    }
-    
-    for(int num = 1; num <= 4; num++) {
-        comp.push_back(num);
-        Choose(pos + 1);
+    for (int num = 1; num <= 4; ++num) {
+        comp.push_back(num); 
+        Choose();
         comp.pop_back();
     }
 }
 
 int main() {
     cin >> str;
-    Choose(0);
+
+    for (char c : str) {
+        if (idx_map.find(c) == idx_map.end()) {
+            idx_map[c] = alpha.size();
+            alpha.push_back(c);
+        }
+    }
+
+    Choose();
     cout << max_num << endl;
     return 0;
 }
-
-// N길이: a~f, +-*
-//알파벳에 1~4 대입하여 최댓값
